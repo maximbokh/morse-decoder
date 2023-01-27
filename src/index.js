93cdfa727cb93a5ac1bdf997cf6f38ea8fb9d6f4
maximbokh <isupermaximus@gmail.com>
2023-01-27 15:32:39 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,28 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let mas = [];
+    let mas1 = [];
+    let result = '';
+
+    for (let i = 0; i < expr.length; i+=10){
+        mas.push(expr.slice(i, i+10));
+    }
+
+    mas.forEach(value => {
+        mas1.push(value.replace(/10/ig, ".").replace(/11/ig, "-").replace(/[0]/g, ''));
+    });
+
+    mas1.forEach(value => {
+        for (let key in MORSE_TABLE) {
+          if (value === key) {
+            result += MORSE_TABLE[key]
+          }
+        } if (value === '**********') {
+            result += ' ';
+        }
+    })
+    return result;
 }
 
 module.exports = {
